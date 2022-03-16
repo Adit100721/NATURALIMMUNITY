680c3f6c8332c2860401bf0e6185fb2e2f19612c
Your Name <you@example.com>
2022-03-16 18:35:05 +0530

    initial commit

---
+++ cart.php
@@ -0,0 +1,317 @@
+ <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="author" content="Sahil Kumar">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Shopping Cart System</title>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css' />
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css' />
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- Template Main CSS File -->
+  
+</head>
+
+<body>
+  <!-- Navbar start -->
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+    <a class="navbar-brand" href="index.php">&nbsp;&nbsp;NATURAL IMMUNITY</a>
+    <button class="navbar-toggler" typ…
+    <?php
+	session_start();
+	require 'config.php';
+
+	// Add products into the cart table
+	if (isset($_POST['pid'])) {
+	  $pid = $_POST['pid'];
+	  $pname = $_POST['pname'];
+	  $pprice = $_POST['pprice'];
+	  $pimage = $_POST['pimage'];
+	  $pcode = $_POST['pcode'];
+	  $pqty = $_POST['pqty'];
+	  $total_price = $pprice * $pqty;
+
+	  $stmt = $conn->prepare('SELECT product_code FROM cart WHERE product_code=?');
+	  $stmt->bind_param('s',$pcode);
+	  $stmt->execute();
+	  $res = $stmt->get_result();
+	  $r = $res->fetch_assoc();
+	  $code = $r['product_code'] ?? '';
+
+	  if (!$code) {
+	    $query = $conn->prepare('INSERT INTO cart (product_name,product_price,product_image,qty,total_price,product_code) VALUES (?,?,?,?,?,?)');
+	    $query->bind_param('ssssss',$pname,$pprice,$pimage,$pqty,$total_price,$pcode);
+	    $query->execute();
+
+	    echo '<div class="alert alert-success alert-dismissible mt-2">
+						  <button type="button" class="close" data-dismiss="alert">&times;</button>
+						  <strong>Item added to your cart!</strong>
+						</div>';
+	  } else {
+	    echo '<div class="alert alert-danger alert-dismissible mt-2">
+						  <button type="button" class="close" data-dismiss="alert">&times;</button>
+						  <strong>Item already added to your cart!</strong>
+						</div>';
+	  }
+	}
+
+	// Get no.of items available in the cart table
+	if (isset($_GET['cartItem']) && isset($_GET['cartItem']) == 'cart_item') {
+	  $stmt = $conn->prepare('SELECT * FROM cart');
+	  $stmt->execute();
+	  $stmt->store_result();
+	  $rows = $stmt->num_rows;
+
+	  echo $rows;
+	}
+
+	// Remove single items from cart
+	if (isset($_GET['remove'])) {
+	  $id = $_GET['remove'];
+
+	  $stmt = $conn->prepare('DELETE FROM cart WHERE id=?');
+	  $stmt->bind_param('i',$id);
+	  $stmt->execute();
+
+	  $_SESSION['showAlert'] = 'block';
+	  $_SESSION['message'] = 'Item removed from the cart!';
+	  header('location:cart.php');
+	}
+
+	// Remove all items at once from cart
+	if (isset($_GET['clear'])) {
+	  $stmt = $conn->prepare('DELETE FROM cart');
+	  $stmt->execute();
+	  $_SESSION['showAlert'] = 'block';
+	  $_SESSION['message'] = 'All Item removed from the cart!';
+	  header('location:cart.php');
+	}
+
+	// Set total price of the product in the cart table
+	if (isset($_POST['qty'])) {
+	  $qty = $_POST['qty'];
+	  $pid = $_POST['pid'];
+	  $pprice = $_POST['pprice'];
+
+	  $tprice = $qty * $pprice;
+
+	  $stmt = $conn->prepare('UPDATE cart SET qty=?, total_price=? WHERE id=?');
+	  $stmt->bind_param('isi',$qty,$tprice,$pid);
+	  $stmt->execute();
+	}
+
+	// Checkout and save customer info in the orders table
+	if (isset($_POST['action']) && isset($_POST['action']) == 'order') {
+	  $name = $_POST['name'];
+	  $email = $_POST['email'];
+	  $phone = $_POST['phone'];
+	  $products = $_POST['products'];
+	  $grand_total = $_POST['grand_total'];
+	  $address = $_POST['address'];
+	  $pmode = $_POST['pmode'];
+
+	  $data = '';
+
+	  $stmt = $conn->prepare('INSERT INTO orders (name,email,phone,address,pmode,products,amount_paid)VALUES(?,?,?,?,?,?,?)');
+	  $stmt->bind_param('sssssss',$name,$email,$phone,$address,$pmode,$products,$grand_total);
+	  $stmt->execute();
+	  $stmt2 = $conn->prepare('DELETE FROM cart');
+	  $stmt2->execute();
+	  $data .= '<div class="text-center">
+								<h1 class="display-4 mt-2 text-danger">Thank You!</h1>
+								<h2 class="text-success">Your Order Placed Successfully!</h2>
+								<h4 class="bg-danger text-light rounded p-2">Items Purchased : ' . $products . '</h4>
+								<h4>Your Name : ' . $name . '</h4>
+								<h4>Your E-mail : ' . $email . '</h4>
+								<h4>Your Phone : ' . $phone . '</h4>
+								<h4>Total Amount Paid : ' . number_format($grand_total,2) . '</h4>
+								<h4>Payment Mode : ' . $pmode . '</h4>
+						  </div>';
+	  echo $data;
+	}
+?>
+[7:20 pm, 07/10/2021] Aditya Dubey: <!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="author" content="Sahil Kumar">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Cart</title>
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.min.css' />
+  <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.9.0/css/all.min.css' />
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+    <!-- Brand -->
+    <a class="navbar-brand" href="index.php"><i class="fas fa-mobile-alt"></i>&nbsp;&nbsp;Mobile Store</a>
+    <!-- Toggler/collapsibe Button -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <!-- Navbar links -->
+    <div class="collapse navbar-collapse" id="collapsibleNavbar">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+          <a class="nav-link active" href="index.html"><i class="fas fa-mobile-alt mr-2"></i>Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="contact.html"><i class="fas fa-th-list mr-2"></i>Contact us</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="checkout.php"><i class="fas fa-money-check-alt mr-2"></i>Checkout</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="cart.php"><i class="fas fa-shopping-cart"></i> <span id="cart-item" class="badge badge-danger"></span></a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-lg-10">
+        <div style="display:<?php if (isset($_SESSION['showAlert'])) {
+  echo $_SESSION['showAlert'];
+} else {
+  echo 'none';
+} unset($_SESSION['showAlert']); ?>" class="alert alert-success alert-dismissible mt-3">
+          <button type="button" class="close" data-dismiss="alert">&times;</button>
+          <strong><?php if (isset($_SESSION['message'])) {
+  echo $_SESSION['message'];
+} unset($_SESSION['showAlert']); ?></strong>
+        </div>
+        <div class="table-responsive mt-2">
+          <table class="table table-bordered table-striped text-center">
+            <thead>
+              <tr>
+                <td colspan="7">
+                  <h4 class="text-center text-info m-0">Products in your cart!</h4>
+                </td>
+              </tr>
+              <tr>
+                <th>ID</th>
+                <th>Image</th>
+                <th>Product</th>
+                <th>Price</th>
+                <th>Quantity</th>
+                <th>Total Price</th>
+                <th>
+                  <a href="action.php?clear=all" class="badge-danger badge p-1" onclick="return confirm('Are you sure want to clear your cart?');"><i class="fas fa-trash"></i>&nbsp;&nbsp;Clear Cart</a>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php
+                require 'config.php';
+                $stmt = $conn->prepare('SELECT * FROM cart');
+                $stmt->execute();
+                $result = $stmt->get_result();
+                $grand_total = 0;
+                while ($row = $result->fetch_assoc()):
+              ?>
+              <tr>
+                <td><?= $row['id'] ?></td>
+                <input type="hidden" class="pid" value="<?= $row['id'] ?>">
+                <td><img src="<?= $row['product_image'] ?>" width="50"></td>
+                <td><?= $row['product_name'] ?></td>
+                <td>
+                  <i class="fas fa-rupee-sign"></i>&nbsp;&nbsp;<?= number_format($row['product_price'],2); ?>
+                </td>
+                <input type="hidden" class="pprice" value="<?= $row['product_price'] ?>">
+                <td>
+                  <input type="number" class="form-control itemQty" value="<?= $row['qty'] ?>" style="width:75px;">
+                </td>
+                <td><i class="fas fa-rupee-sign"></i>&nbsp;&nbsp;<?= number_format($row['total_price'],2); ?></td>
+                <td>
+                  <a href="action.php?remove=<?= $row['id'] ?>" class="text-danger lead" onclick="return confirm('Are you sure want to remove this item?');"><i class="fas fa-trash-alt"></i></a>
+                </td>
+              </tr>
+              <?php $grand_total += $row['total_price']; ?>
+              <?php endwhile; ?>
+              <tr>
+                <td colspan="3">
+                  <a href="services.php" class="btn btn-success"><i class="fas fa-cart-plus"></i>&nbsp;&nbsp;Continue 
+                  Shopping</a>
+                </td>
+                <td colspan="2"><b>Grand Total</b></td>
+                <td><b><i class="fas fa-rupee-sign"></i>&nbsp;&nbsp;<?= number_format($grand_total,2); ?></b></td>
+                <td>
+                  <a href="checkout.php" class="btn btn-info <?= ($grand_total > 1) ? '' : 'disabled'; ?>"><i class="far fa-credit-card"></i>&nbsp;&nbsp;Checkout</a>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js'></script>
+
+  <script type="text/javascript">
+  $(document).ready(function() {
+
+    // Change the item quantity
+    $(".itemQty").on('change', function() {
+      var $el = $(this).closest('tr');
+
+      var pid = $el.find(".pid").val();
+      var pprice = $el.find(".pprice").val();
+      var qty = $el.find(".itemQty").val();
+      location.reload(true);
+      $.ajax({
+        url: 'action.php',
+        method: 'post',
+        cache: false,
+        data: {
+          qty: qty,
+          pid: pid,
+          pprice: pprice
+        },
+        success: function(response) {
+          console.log(response);
+        }
+      });
+    });
+
+    // Load total no.of items added in the cart and display in the navbar
+    load_cart_item_number();
+
+    function load_cart_item_number() {
+      $.ajax({
+        url: 'action.php',
+        method: 'get',
+        data: {
+          cartItem: "cart_item"
+        },
+        success: function(response) {
+          $("#cart-item").html(response);
+        }
+      });
+    }
+  });
+  </script>
+</body>
+
+</html>
---
+++ services.php
@@ -0,0 +1,221 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="author" content="Sahil Kumar">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Shopping Cart System</title>
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Roboto:300,300i,400,400i,500,500i,700,700i&display=swap" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/animate.css/animate.min.css" rel="stylesheet">
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+  
+</head>
+
+<body>
+  <!-- Navbar start -->
+  <header id="header" class="fixed-top d-flex align-items-center ">
+    <div class="container d-flex justify-content-between align-items-center">
+
+      <div class="logo">
+        <h1 class="text-light"><a href="index.html"><span>NATURAL IMMUNITY</span></a></h1>
+        <!-- Uncomment below if you prefer to use an image logo -->
+        <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+      </div>
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="" href="index.html">Home</a></li>
+          <li><a href="about.html">INFO</a></li>
+          <li><a class="active" href="services.html">Services</a></li>
+          <li><a href="contact.html">Contact Us</a></li>
+          <li><a href="checkout.php">Checkout</a></li>
+          <li><a href="cart.php"> <i class="fa fa-shopping-cart" style="font-size:48px;color:White"></i><span id="cart-item" class="badge badge-danger"></span> </a></li>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+
+    </div>
+      </header>
+      <!-- End Header -->
+  <!-- Navbar end -->
+<section>
+  <!-- Displaying Products Start -->
+  <div class="container" style="position:relative; bottom:-50px; left:100px;margin:5px; gap:50px;">
+    <div id="message"></div>
+    <div class="row mt-2 pb-3">
+      <?php
+  			include 'config.php';
+  			$stmt = $conn->prepare('SELECT * FROM product');
+  			$stmt->execute();
+  			$result = $stmt->get_result();
+  			while ($row = $result->fetch_assoc()):
+  		?>
+      <div class="col-sm-6 col-md-4 col-lg-3 mb-2">
+        <div class="card-deck">
+          <div class="card p-2 border-secondary mb-2">
+            <img src="<?= $row['product_image'] ?>" class="card-img-top" height="250">
+            <div class="card-body p-1">
+              <h4 class="card-title text-center text-info"><?= $row['product_name'] ?></h4>
+              <h5 class="card-text text-center text-danger"><i class="fas fa-rupee-sign"></i>&nbsp;&nbsp;<?= number_format($row['product_price'],2) ?>/-</h5>
+
+            </div>
+            <div class="card-footer p-1">
+              <form action="" class="form-submit">
+                <div class="row p-2">
+                  <div class="col-md-6 py-1 pl-4">
+                    <b>Quantity : </b>
+                  </div>
+                  <div class="col-md-6">
+                    <input type="number" class="form-control pqty" value="<?= $row['product_qty'] ?>">
+                  </div>
+                </div>
+                <input type="hidden" class="pid" value="<?= $row['id'] ?>">
+                <input type="hidden" class="pname" value="<?= $row['product_name'] ?>">
+                <input type="hidden" class="pprice" value="<?= $row['product_price'] ?>">
+                <input type="hidden" class="pimage" value="<?= $row['product_image'] ?>">
+                <input type="hidden" class="pcode" value="<?= $row['product_code'] ?>">
+                <button class="btn btn-info btn-block addItemBtn"><i class="fas fa-cart-plus"></i>&nbsp;&nbsp;Add to
+                  cart</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <?php endwhile; ?>
+    </div>
+  </div>
+  </section>
+
+<section>
+<br>
+<div class="container" style="position:absolute; bottom:-650px; left:100px;margin:5px; gap:50px;color:blue; ">
+  <div class="container">
+    <div class="row">
+
+
+      <div class="col-lg-3 col-md-6 footer-contact">
+        <h4>Contact Us</h4>
+        <p>
+          A108 Adam Street <br>
+          New York, NY 535022<br>
+          United States <br><br>
+          <strong>Phone:</strong> +1 5589 55488 55<br>
+          <strong>Email:</strong> info@example.com<br>
+        </p>
+
+      </div>
+
+      <div class="col-lg-3 col-md-6 footer-info">
+        <h3>About NATURAL IMMUNITY</h3>
+        <p>NATURAL IMMUNITY IS WEBSITE WITCH TELLS HOW WE CAN INCREASE OUR IMMUNITY WITH NATURAL THINGS
+        </p>
+        <div class="social-links mt-3">
+          <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
+          <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
+          <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
+          <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
+        </div>
+      </div>
+
+    </div>
+  </div>
+</div>
+
+<div class="container" style="position:absolute; bottom:-700px; left:100px;margin:5px; gap:50px;color:blue;">
+  <div class="copyright">
+    &copy; Copyright <strong><span>NATURAL IMMUNITY</span></strong>. All Rights Reserved
+  </div>
+  <div class="credits">
+    <!-- All the links in the footer should remain intact. -->
+    <!-- You can delete the links only if you purchased the pro version. -->
+    <!-- Licensing information: https://bootstrapmade.com/license/ -->
+    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-template-corporate-moderna/ -->
+    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
+  </div>
+</div>
+    </section>
+<a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+
+
+  
+
+  <!-- Displaying Products End -->
+
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/js/bootstrap.min.js'></script>
+
+  <script type="text/javascript">
+  $(document).ready(function() {
+
+    // Send product details in the server
+    $(".addItemBtn").click(function(e) {
+      e.preventDefault();
+      var $form = $(this).closest(".form-submit");
+      var pid = $form.find(".pid").val();
+      var pname = $form.find(".pname").val();
+      var pprice = $form.find(".pprice").val();
+      var pimage = $form.find(".pimage").val();
+      var pcode = $form.find(".pcode").val();
+
+      var pqty = $form.find(".pqty").val();
+
+      $.ajax({
+        url: 'action.php',
+        method: 'post',
+        data: {
+          pid: pid,
+          pname: pname,
+          pprice: pprice,
+          pqty: pqty,
+          pimage: pimage,
+          pcode: pcode
+        },
+        success: function(response) {
+          $("#message").html(response);
+          window.scrollTo(0, 0);
+          load_cart_item_number();
+        }
+      });
+    });
+
+    // Load total no.of items added in the cart and display in the navbar
+    load_cart_item_number();
+
+    function load_cart_item_number() {
+      $.ajax({
+        url: 'action.php',
+        method: 'get',
+        data: {
+          cartItem: "cart_item"
+        },
+        success: function(response) {
+          $("#cart-item").html(response);
+        }
+      });
+    }
+  });
+
+  </script>
+  
+</body>
+
+</html>
